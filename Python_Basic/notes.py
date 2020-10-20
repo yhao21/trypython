bc5c69c96a12236796302e9a10b7fad39a5c7401
yhao21 <13701304462@163.com>
2020-10-20 01:45:33 -0400

    os.listdir

---
+++ Python_Basic/notes.py
@@ -0,0 +1,13 @@
+'''
+=========================
+Check empty folder
+=========================
+
+It will print yes if folder is empty:
+
+    if not os.listdir(os.path.join(os.getcwd(), 'haha')):
+        print('yes')
+
+NB: os.listdir lists all files in this directory. 
+
+'''
